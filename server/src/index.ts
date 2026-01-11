75f4c8880cd44f800886665718271bed981f323e
YounesNaitBakkou <naitbakkou.younes@gmail.com>
2026-01-11 22:16:08 +0100

    feat(cors): setup cors with origin of localhost:5173

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 8001;
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+    }),
+);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");

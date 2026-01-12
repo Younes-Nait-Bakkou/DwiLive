04d3b9368f4d389cfe9ae8add67e6b6079c8e7a6
YounesNaitBakkou <naitbakkou.younes@gmail.com>
2026-01-12 20:06:04 +0100

    feat(index): start server only when db connected

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 const app = express();
 const PORT = 8001;
@@ -14,6 +15,11 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const startServer = async () => {
+    await connectDB();
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+};
+
+startServer();

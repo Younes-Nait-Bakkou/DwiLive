5a10c6cef1475e084d625dc068e02b5e6d8ed0e8
YounesNaitBakkou <naitbakkou.younes@gmail.com>
2026-01-13 23:05:39 +0100

    feat(models): add a Message model

---
+++ server/src/models/Message.ts
@@ -0,0 +1,44 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IMessage extends Document {
+    roomId: mongoose.Types.ObjectId;
+    senderId: mongoose.Types.ObjectId;
+    content: string;
+    type: "text" | "image";
+    createdAt: Date;
+}
+
+const messageSchema = new Schema<IMessage>(
+    {
+        roomId: {
+            type: Schema.Types.ObjectId,
+            ref: "Room",
+            required: true,
+            index: true,
+        },
+        senderId: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            enum: ["text", "image"],
+            default: "text",
+        },
+    },
+    {
+        timestamps: { createdAt: true, updatedAt: false },
+        versionKey: false,
+        id: true,
+        toJSON: {
+            virtuals: true,
+        },
+    },
+);
+
+export default mongoose.model<IMessage>("Message", messageSchema);

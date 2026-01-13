d732d1174f6ac9976495ed056c48aaae641c53f1
YounesNaitBakkou <naitbakkou.younes@gmail.com>
2026-01-13 20:59:31 +0100

    feat(config): handle new envs, nest JWT config inside jwt obj

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -14,11 +14,22 @@ const configSchema = z.object({
     MONGODB_URI: z.string(),
     CORS_ORIGIN: z.string().default("http://localhost:5173"),
     JWT_SECRET: z.string().default("secret"),
+    JWT_EXPIRES_IN: z.string().default("30d"),
+    JWT_HEADER_PREFIX: z.string().default("Bearer"),
 });
 
 const config = configSchema.parse(process.env);
 
 export default {
-    ...config,
+    NODE_ENV: config.NODE_ENV,
+    PORT: config.PORT,
+    MONGODB_URI: config.MONGODB_URI,
+    CORS_ORIGIN: config.CORS_ORIGIN,
     ROOT_DIR: path.join(__dirname, "../.."),
+    jwt: {
+        secret: config.JWT_SECRET,
+        expiresIn: config.JWT_EXPIRES_IN,
+        prefix: config.JWT_HEADER_PREFIX,
+    },
 };
+

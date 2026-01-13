3fba464fd2702ac10fa9ed2081f6cb6704668e46
YounesNaitBakkou <naitbakkou.younes@gmail.com>
2026-01-14 00:41:15 +0100

    fix(routes): move auth routes to v1, rename auth.ts to auth.routes.ts

--- server/src/routes/v1/auth.routes.ts
+++ server/src/routes/v1/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register, login } from "../controllers/auth.js";
+import { register, login } from "../../controllers/auth.js";
 
 const router: Router = Router();
 

61a1892186e7ff0a11cf874daaecb56492ab1052
YounesNaitBakkou <naitbakkou.younes@gmail.com>
2026-01-14 23:39:10 +0100

    feat(middlewares): add validateResource middleware to validate request body and query params based on zod schema

---
+++ server/src/middlewares/validateResource.ts
@@ -0,0 +1,27 @@
+import type { RequestHandler } from "express";
+import { ZodError } from "zod";
+import type { AnyZodObject } from "zod/v3";
+
+export const validate =
+    (schema: AnyZodObject): RequestHandler =>
+    async (req, res, next) => {
+        try {
+            await schema.parseAsync({
+                body: req.body,
+                query: req.query,
+                params: req.params,
+            });
+
+            return next();
+        } catch (error) {
+            if (error instanceof ZodError) {
+                res.status(400).json({
+                    status: "fail",
+                    errors: error.issues,
+                });
+                return;
+            }
+
+            return next(error);
+        }
+    };

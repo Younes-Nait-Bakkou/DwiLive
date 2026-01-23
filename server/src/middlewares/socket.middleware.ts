4ecb122ad848e2eca45eb22651e190cb8c346db5
YounesNaitBakkou <naitbakkou.younes@gmail.com>
2026-01-23 23:51:25 +0100

    feat: Update socket.middleware.ts for new SocketEvent structure
    Updates the jsonParseMiddleware to use the newly structured SocketEvent object,
    accessing events via SocketEvent.Client or SocketEvent.Server.

--- server/src/middlewares/socket.middleware.ts
+++ server/src/middlewares/socket.middleware.ts
@@ -4,8 +4,10 @@ export const jsonParseMiddleware = (
     packet: SocketPacket,
     next: (err?: Error) => void,
 ) => {
+    console.log("packet", packet);
     for (let i = 1; i < packet.length; i++) {
         const arg = packet[i];
+        console.log("packet type", typeof arg);
 
         if (typeof arg === "string") {
             try {

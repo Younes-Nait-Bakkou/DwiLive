6072b5b2733408fd016b4b3d2e7b54540662a58e
YounesNaitBakkou <naitbakkou.younes@gmail.com>
2026-01-25 20:54:22 +0100

    feat(domain): add conversation domain

--- server/src/shared/domains/index.ts
+++ server/src/shared/domains/index.ts
@@ -1,2 +1,4 @@
 export * as AuthDomain from "./auth.js";
 export * as UserDomain from "./user.js";
+export * as ConversationDomain from "./conversation.js";
+export * as MessageDomain from "./message.js";

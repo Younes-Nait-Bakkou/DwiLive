36765ea43c3bda033ac5ca31e2059f54bf7c9828
YounesNaitBakkou <naitbakkou.younes@gmail.com>
2026-01-25 22:53:10 +0100

    feat(mappers): update message mapper to handle system messages

--- server/src/mappers/message.mapper.ts
+++ server/src/mappers/message.mapper.ts
@@ -1,13 +1,37 @@
 import type { IMessage } from "../models/Message.js";
+import type { IUser } from "../models/User.js";
 import type { MessageDomain } from "../shared/domains/index.js";
+import { isPopulated } from "../utils/typeGuards.js";
+import { UserMapper } from "./index.js";
 
 export const toMessageDTO = (message: IMessage): MessageDomain.MessageDTO => {
-    return {
+    const conversationId = message.conversation!.toString();
+
+    const base = {
         id: message.id,
-        conversationId: message.conversation?.toString() || "",
-        senderId: message.sender?.toString() || "",
-        content: message.content,
-        type: message.type,
+        conversationId: conversationId,
         createdAt: message.createdAt.toISOString(),
+    };
+
+    if (message.type === "system" && message.metadata) {
+        return {
+            ...base,
+            type: "system",
+            sender: null,
+            content: message.content,
+            metadata: message.metadata,
+        } as MessageDomain.SystemMessageDTO;
+    }
+
+    if (!message.sender || !isPopulated<IUser>(message.sender)) {
+        throw new Error(`Message ${message.id} has no populated sender!`);
+    }
+
+    return {
+        ...base,
+        type: message.type as "text" | "image",
+        content: message.content,
+        sender: UserMapper.toUserDTO(message.sender),
+        metadata: null,
     };
 };

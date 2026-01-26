ae693558891ca774c462b8f0a454c3f4073c562e
YounesNaitBakkou <naitbakkou.younes@gmail.com>
2026-01-26 23:09:07 +0100

    fix(utils): correct typeguard for isPopulated

--- server/src/utils/typeGuards.ts
+++ server/src/utils/typeGuards.ts
@@ -1,15 +1,17 @@
-import type { Document, PopulatedDoc } from "mongoose";
+import { Document, Types, type PopulatedDoc } from "mongoose";
 
 /**
- * Type Guard: Checks if a Mongoose field is fully populated (Document)
- * or just an ID (ObjectId/string).
+ * Type Guard: Checks if a Mongoose field is a fully populated Document.
+ * Returns true if it is a Document, false if it is an ID (String/ObjectId).
  */
 export const isPopulated = <T extends Document>(
     doc: PopulatedDoc<T> | undefined | null,
 ): doc is T => {
-    return (
-        !!doc && // Ensure not null/undefined
-        typeof doc === "object" && // Ensure it's an object (Documents are objects)
-        "_id" in doc // Ensure it has an '_id' (Ref IDs do not)
-    );
+    if (!doc) return false;
+
+    if (doc instanceof Types.ObjectId) return false;
+
+    if (typeof doc === "string") return false;
+
+    return typeof (doc as T).populated === "function";
 };

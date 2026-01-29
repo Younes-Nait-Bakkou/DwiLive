a73bd9f08ca34225e7da77ee3aabb7032c67acc8
YounesNaitBakkou <naitbakkou.younes@gmail.com>
2026-01-29 20:04:55 +0100

    fix(server): remove files that are moved to the shared package

--- packages/server/src/types/socket.ts
+++
@@ -1,9 +0,0 @@
-export type SocketResponse =
-    | { status: "OK"; data: unknown }
-    | { status: "ERROR"; error: string; code?: string };
-
-export enum ErrorCodes {
-    UNAUTHORIZED = "UNAUTHORIZED",
-    VALIDATION_ERROR = "VALIDATION_ERROR",
-    INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR",
-}
